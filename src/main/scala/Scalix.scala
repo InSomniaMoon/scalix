e1b323692d550cc0d38d965465532d45d62cea26
ATHIMON Clement <clement.athimon@imt-atlantique.net>
2023-01-24 18:53:53 +0100

    merging manually for find actor movies

--- src/main/scala/Scalix.scala
+++ src/main/scala/Scalix.scala
@@ -7,6 +7,8 @@ import org.json4s.native.JsonMethods._
 trait Config(val api_key: String)
 
 object Scalix extends App, Config("65c251744206a64af3ad031e4d5a4a48") {
+  implicit val formats: Formats = DefaultFormats
+  case class MovieLight(id: Int, title:String);
 
   /**
    * utils function
@@ -35,6 +37,19 @@ object Scalix extends App, Config("65c251744206a64af3ad031e4d5a4a48") {
     Option(compact(render(results(0) \ "id")).toInt)
   }
 
+
+  def findActorMovies(actorId : Int): Set[(Int, String)] = {
+    val data = getData(s"/person/$actorId/movie_credits", "")
+    if (data.getClass == JNothing.getClass) {
+      return Set((0, "No movie for this actor"));
+    }
+    val movies = (data \ "cast").extract[List[MovieLight]];
+    var results = Set[(Int, String)]();
+    movies.map(e => results += (e.id,e.title));
+    return results;
+
+  }
+
   def findMovieDirector(movieId: Int): Option[(Int, String)] = {
     val data = getData(s"/movie/$movieId/credits")
 
@@ -48,7 +63,8 @@ object Scalix extends App, Config("65c251744206a64af3ad031e4d5a4a48") {
   println(id)
   val id2 = findActorId("Brad", "Pittt")
   println(id2)
-
+  val moviesBradPitt = findActorMovies(287);
+  println(moviesBradPitt);
   val dir = findMovieDirector(550)
   println(dir)
 
