bda913b65795b2441ac0584d48c56fcd097dcec7
ATHIMON Clement <clement.athimon@imt-atlantique.net>
2023-01-25 09:00:25 +0100

    end of collaboration

--- src/main/scala/Scalix.scala
+++ src/main/scala/Scalix.scala
@@ -19,7 +19,6 @@ object Scalix extends App, Config("65c251744206a64af3ad031e4d5a4a48") {
   def getData(uri: String, query: String = ""): JValue = {
     val url = s"https://api.themoviedb.org/3$uri?api_key=$api_key&language=fr-FR$query"
     val source = Source.fromURL(url)
-    println(url)
     val contents  = source.mkString
     parse(contents)
   }
@@ -63,12 +62,22 @@ object Scalix extends App, Config("65c251744206a64af3ad031e4d5a4a48") {
     val id1 = findActorId(actor1.firstName,actor1.lastName);
     val id2 = findActorId(actor2.firstName,actor2.lastName);
     val data = getData("/discover/movie",s"&with_cast=${id1.head},${id2.head}");
-    println(data);
+    if (data.getClass == JNothing.getClass) {
+      return Set(("No movies for those two actors", "No movie for those actors"));
+    }
+    val totalResults = compact(render(data\"total_results")).toInt;
+    if(totalResults > 0) {
+      val movies = (data \ "results").extract[List[MovieLight]];
+      var results = Set[(String, String)]();
+      movies.map((movie) => {
+        var director = findMovieDirector(movie.id);
+        results += (director.head(1),movie.title)
+      });
+      results;
+    }else{
+      Set(("No movies for those two actors", "No movie for those actors"));
+    }
 
-    //val movies = (data \ "cast").extract[List[MovieLight]];
-    var results = Set[(String, String)]();
-    //movies.map(e => results += (e.id,e.title));
-    return results;
 
   }
 
@@ -84,5 +93,15 @@ object Scalix extends App, Config("65c251744206a64af3ad031e4d5a4a48") {
   val bradPitt = new FullName("Brad","Pitt");
   val claireForlani = new FullName("Claire","Forlani");
   val collaborationPittForlani = collaboration(bradPitt,claireForlani);
+  println(collaborationPittForlani);
+
+  val mattDamon = new FullName("Matt", "Damon");
+  val collaborationPittDamon = collaboration(bradPitt,mattDamon);
+  println(collaborationPittDamon);
+
+  val pierreNiney = new FullName("Pierre", "Niney");
+  val collaborationPittNiney = collaboration(bradPitt,pierreNiney);
+  println(collaborationPittNiney);
+
 
 }
